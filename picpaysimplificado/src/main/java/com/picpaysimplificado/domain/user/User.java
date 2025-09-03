c0ae9605bfcf50d13472b2278f4773f812b7c8e5
Maykon Carvalho <carvalhomaykon566@gmail.com>
2025-09-03 18:38:12 -0300

    Picpaysimplificado: create User entity

---
+++ picpaysimplificado/src/main/java/com/picpaysimplificado/domain/user/User.java
@@ -0,0 +1,39 @@
+package com.picpaysimplificado.domain.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity(name="users")
+@Table(name="users")
+// Os campos abaixos foram criados utilizando uma dependência chamada de lombok
+@Getter
+@Setter
+@AllArgsConstructor // Para criar o construtor que receba todos os parâmetros da classe
+@EqualsAndHashCode(of="id")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+    @Column(unique=true)
+    private String document;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+    private BigDecimal balance;
+
+    @Enumerated(EnumType.STRING)
+    private UserType userType;
+
+    public User() {}
+}
