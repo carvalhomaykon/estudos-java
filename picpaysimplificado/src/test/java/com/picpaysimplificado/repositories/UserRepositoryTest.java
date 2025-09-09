64ce9be8ae65c172d9a782f1b6f91d2a4b329f17
carvalhomaykon <carvalhomaykon566@gmail.com>
2025-09-09 12:15:05 -0300

    Picpaysimplificado: Implementation of test

---
+++ picpaysimplificado/src/test/java/com/picpaysimplificado/repositories/UserRepositoryTest.java
@@ -0,0 +1,56 @@
+package com.picpaysimplificado.repositories;
+
+import com.picpaysimplificado.domain.user.User;
+import com.picpaysimplificado.domain.user.UserType;
+import com.picpaysimplificado.dtos.UserDTO;
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@ActiveProfiles("test")
+class UserRepositoryTest {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    EntityManager entityManager; // Utilizado para criar um usuário fake para teste
+
+    @Test // Dizer que é um teste
+    @DisplayName("Should get User successfully from DB") // Descrição do teste
+    void findUserByDocumentCase1() {
+        String document = "99999999901";
+        UserDTO data = new UserDTO("Maykon", "Teste", document, new BigDecimal(10.0), "teste@gmail.com", "4444", UserType.COMMON);
+        this.createUser(data);
+
+        Optional<User> result = this.userRepository.findUserByDocument(document);
+
+        assertThat(result.isPresent()).isTrue();
+    }
+
+    @Test // Dizer que é um teste
+    @DisplayName("Should not get User from DB when user not exists") // Descrição do teste
+    void findUserByDocumentCase2() {
+        String document = "99999999901";
+
+        Optional<User> result = this.userRepository.findUserByDocument(document);
+
+        assertThat(result.isEmpty()).isTrue();
+    }
+
+    private User createUser(UserDTO data){
+        User newUser = new User(data);
+        this.entityManager.persist(newUser);
+        return newUser;
+    }
+
+}
