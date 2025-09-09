98fffe89668662e71f5e3ed3b2dd8ca41e863803
carvalhomaykon <carvalhomaykon566@gmail.com>
2025-09-09 12:14:16 -0300

    Picpaysimplificado: Refactoration of entites and services

---
+++ picpaysimplificado/src/main/java/com/picpaysimplificado/services/AuthorizationService.java
@@ -0,0 +1,35 @@
+package com.picpaysimplificado.services;
+
+import com.picpaysimplificado.domain.user.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.math.BigDecimal;
+import java.util.Map;
+
+@Service
+public class AuthorizationService {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public boolean authorizeTransaction(User sender, BigDecimal value){
+        ResponseEntity<Map> authorizationResponse = restTemplate.getForEntity("https://util.devi.tools/api/v2/authorize", Map.class);
+        if (authorizationResponse.getStatusCode() == HttpStatus.OK){
+            Map<String, Object> body = authorizationResponse.getBody();
+            Map<String, Object> data = (Map<String, Object>) body.get("data");
+
+            if ("true".equals(String.valueOf(data.get("authorization")))) {
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        return false;
+    }
+
+}
